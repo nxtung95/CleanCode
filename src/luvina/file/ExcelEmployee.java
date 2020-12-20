e88b6471410981d301c5341dce62763178f4cef2
TungNX <nxtung95@gmail.com>
2020-12-20 23:36:11 +0700

    Read excel file

---
+++ src/luvina/file/ExcelEmployee.java
@@ -0,0 +1,14 @@
+package luvina.file;
+
+import java.util.List;
+
+public class ExcelEmployee implements IEmployeeFile {
+
+	@Override
+	public List<String> readFile(String filePath) {
+		// TODO Auto-generated method stub
+		// Logic read excel file
+		return null;
+	}
+
+}
